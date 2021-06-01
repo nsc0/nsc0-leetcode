0fca45f1d91fdb183b2d79ab8c7041d477a86c65
cuican2020g <nsyk46@gmail.com>
2021-06-01 13:43:13 +0800

    300. 递归解法

--- 300.最长递增子序列.js
+++ 300.最长递增子序列.js
@@ -9,7 +9,18 @@
  * @param {number[]} nums
  * @return {number}
  */
-var lengthOfLIS = function (nums) {}
+var lengthOfLIS = function (nums, dp = [1]) {
+  for (let i = 1; i < nums.length; i++) {
+    dp[i] = 1
+    for (let j = 0; j < i; j++) {
+      //   nums[i] > nums[j] && (dp[i] = Math.max(dp[i], dp[j] + 1))
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1)
+      }
+    }
+  }
+  return Math.max(...dp)
+}
 // @lc code=end
 
-console.log(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]))
+console.log(lengthOfLIS([4, 10, 4, 3, 8, 9]))
